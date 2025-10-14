f87b7d02043a846e6be2192ce2363ac928f34880
Krishna <kgsing15@gmail.com>
2025-10-14 20:53:39 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
 
-function App() {
+export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-        </Routes>
+        <BrowserRouter basename="/my-app">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
-
-export default App;
